0d341ceac32b85590a898ca786f189907848741c
Tim Allen <tim.allen@marketplacer.com>
2022-12-06 12:17:35 +1100

    day part 2

--- day_3_rucksack/index.js
+++ day_3_rucksack/index.js
@@ -2,6 +2,15 @@ const fs = require("fs");
 const readline = require("readline");
 const assert = require('assert');
 
+
+const A = "A".charCodeAt(0);
+const Z = "Z".charCodeAt(0);
+const a = "a".charCodeAt(0);
+const z = "z".charCodeAt(0);
+function priority(code) {
+  return (a <= code && code <= z) ? code - a + 1 : code - A + 1 + 26;
+}
+
 async function main() {
   const stream = fs.createReadStream("./input.txt");
 
@@ -11,31 +20,33 @@ async function main() {
   });
 
   let sum = 0;
+  const lines = [];
+  let groups = 0;
+  let linecount = 0;
   for await (const line of rl) {
-    const length = line.length;
-    const [first, second] = [
-      line.substring(0, length / 2),
-      line.substring(length / 2, length),
-    ];
+    linecount++;
+    lines.push(line)
+    if (lines.length < 3) {
+      continue;
+    }
+
+    groups++;
+
+    const [first, second, third ] = lines;
+    lines.length = 0;
+
+    console.log({ first, second, third })
 
     let common;
     for (let c of first) {
-      if (second.includes(c)) {
+      if (second.includes(c) && third.includes(c)) {
         common = c;
         break;
       }
     }
     const code = common.charCodeAt(0);
-    const A = "A".charCodeAt(0);
-    const Z = "Z".charCodeAt(0);
-    const a = "a".charCodeAt(0);
-    const z = "z".charCodeAt(0);
-
-    const priority = (a <= code && code <= z) ? code - a + 1 : code - A + 1 + 26;
-    sum += priority;
-    console.log(line);
-    console.log({ first, second, common, priority, sum });
-    assert(priority > 0);
+    sum += priority(code);
+    console.log({ common, priority: priority(code), sum, groups, linecount })
   }
 }
 
