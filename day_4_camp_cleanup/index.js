9e1f1f6d91951a52a04a23b29e4ef79af87d8237
Tim Allen <tim.allen@marketplacer.com>
2022-12-06 17:12:00 +1100

    day 4 part 2

--- day_4_camp_cleanup/index.js
+++ day_4_camp_cleanup/index.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
 const readline = require('readline')
 
+/*
 function fullyContained (first, second, third, fourth) {
   if (first >= third && second <= fourth) return true
   if (third >= first && fourth <= second) return true
+  return false
+}
+*/
+
+// 49-49,3-48
+function overlaps (first, second, third, fourth) {
+  if (first >= third && first <= fourth) return true
+  if (second >= third && second <= fourth) return true
+  if (third >= first && third <= second) return true
+  if (fourth >= first && fourth <= second) return true
   return false
 }
 
@@ -19,8 +30,8 @@ async function main () {
   for await (const line of rl) {
     console.log(line)
     const [first, second, third, fourth] = /(\d+)-(\d+),(\d+)-(\d+)/.exec(line).slice(1).map(entry => parseInt(entry))
-    if (fullyContained(first, second, third, fourth)) total++
-    console.log({ first, second, third, fourth, contained: fullyContained(first, second, third, fourth), total })
+    if (overlaps(first, second, third, fourth)) total++
+    console.log({ first, second, third, fourth, overlaps: overlaps(first, second, third, fourth), total })
   }
 }
 
