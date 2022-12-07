338f0325021e847b9ffd05ba0b5b734ea195c1ff
Tim Allen <tim.allen@marketplacer.com>
2022-12-07 13:54:49 +1100

    day 6 part 2

--- day_6_tuning_trouble/index.js
+++ day_6_tuning_trouble/index.js
@@ -4,18 +4,19 @@ const readline = require('readline')
 let prev = []
 function parseLine (line) {
   let count = 0
+  const DISTINCT_CHARS = 14
   for (const char of line) {
     count++
-    if (prev.size >= 4) prev.shift(char)
+    if (prev.size >= DISTINCT_CHARS) prev.shift(char)
     const already = prev.indexOf(char)
     if (already >= 0) {
       prev = prev.slice(already + 1)
     }
-    if (already === -1 && prev.length === 3) break
+    if (already === -1 && prev.length === DISTINCT_CHARS - 1) break
     prev.push(char)
     console.log(char, already, prev)
   }
-  console.log(count, line.slice(count - 4, count))
+  console.log(count, line.slice(count - DISTINCT_CHARS, count))
 }
 
 async function main () {
