b5c61985593badb92f87cc82bbb6bf51d9d1e115
Tim Allen <tim.allen@marketplacer.com>
2022-12-12 17:40:46 +1100

    day 9 part 2

--- day_9_rope_bridge/index.js
+++ day_9_rope_bridge/index.js
@@ -1,35 +1,45 @@
 const fs = require('fs')
 const readline = require('readline')
 
-const head = [0, 0]
-const tail = [0, 0]
+const NUM_KNOTS = 10
+const knots = Array.from(Array(NUM_KNOTS), () => [0, 0])
+
 const tailPositions = new Set()
+const tail = knots[NUM_KNOTS - 1]
 
 function parseLine (line) {
   const [, direction, magnitude] = /([UDRL])\s(\d+)/.exec(line)
 
-  let iterations = magnitude
-  while (iterations-- > 0) {
-    if (direction === 'U') {
-      head[1]++
+  for (let iterations = magnitude; iterations > 0; iterations--) {
+    for (let knotIndex = 0; knotIndex < NUM_KNOTS - 1; knotIndex++) {
+      const current = knots[knotIndex]
+      const next = knots[knotIndex + 1]
+      // The line move only applies to the head
+      if (knotIndex === 0) {
+        if (direction === 'U') {
+          current[1]++
+        }
+        if (direction === 'D') {
+          current[1]--
+        }
+        if (direction === 'R') {
+          current[0]++
+        }
+        if (direction === 'L') {
+          current[0]--
+        }
+      }
+      const nextXDiff = current[0] - next[0]
+      const nextYDiff = current[1] - next[1]
+      const nextMoves = Math.abs(nextXDiff) >= 2 || Math.abs(nextYDiff) >= 2
+      // console.log({ nextMoves, current, next, nextXDiff, nextYDiff, line, knots })
+      if (nextMoves) {
+        if (Math.abs(nextXDiff) >= 1) next[0] += (nextXDiff > 0 ? 1 : -1)
+        if (Math.abs(nextYDiff) >= 1) next[1] += (nextYDiff > 0 ? 1 : -1)
+      }
+      if (!nextMoves) break
+      tailPositions.add(`${tail[0]},${tail[1]}`)
     }
-    if (direction === 'D') {
-      head[1]--
-    }
-    if (direction === 'R') {
-      head[0]++
-    }
-    if (direction === 'L') {
-      head[0]--
-    }
-    const tailXDiff = head[0] - tail[0]
-    const tailYDiff = head[1] - tail[1]
-    const tailMoves = Math.abs(tailXDiff) >= 2 || Math.abs(tailYDiff) >= 2
-    if (tailMoves) {
-      if (Math.abs(tailXDiff) >= 1) tail[0] += (tailXDiff > 0 ? 1 : -1)
-      if (Math.abs(tailYDiff) >= 1) tail[1] += (tailYDiff > 0 ? 1 : -1)
-    }
-    tailPositions.add(`${tail[0]},${tail[1]}`)
   }
 }
 
@@ -43,7 +53,6 @@ async function main () {
 
   for await (const line of rl) {
     parseLine(line)
-    // console.log({ line, head, tail, tailPositions })
   }
   console.log({ size: tailPositions.size })
 }
