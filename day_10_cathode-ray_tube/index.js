b28706536b0be16d85bbea4a4c51f778ec91001b
Tim Allen <tim.allen@marketplacer.com>
2022-12-16 10:56:52 +1100

    day 10 part 2

--- day_10_cathode-ray_tube/index.js
+++ day_10_cathode-ray_tube/index.js
@@ -1,26 +1,35 @@
 const fs = require('fs')
 const readline = require('readline')
 
+const LINE_WIDTH = 40
+
 let X = 1
 let cycle = 0
-let total = 0
 
-const interesting = [20, 60, 100, 140, 180, 220]
+function printCycle (cycle, X) {
+  const spriteMinPos = Math.max(0, X - 1)
+  const spriteMaxPos = X + 1
+  const pixelDrawn = (cycle % LINE_WIDTH) - 1
+
+  if (spriteMinPos <= pixelDrawn && pixelDrawn <= spriteMaxPos) printChar('#')
+  else printChar('.')
+}
 
 function parseLine (line) {
   const [, instruction, operand] = /(noop|addx)\s?(-?\d+)?/.exec(line)
   cycle += instruction === 'addx' ? 2 : 1
 
-  // add signal strength to total?
-  if (interesting.includes(cycle)) total += cycle * X
+  if (instruction === 'addx') printCycle(cycle - 1, X) // might need to handle prev cycle
+  printCycle(cycle, X)
 
-  // prev cycle
-  else if (interesting.includes(cycle - 1) && instruction === 'addx') {
-    total += X * (cycle - 1)
-  }
+  if (instruction === 'addx') X += parseInt(operand) // increment register
+}
 
-  // increment register
-  if (instruction === 'addx') X += parseInt(operand)
+let pixel = 1
+function printChar (character) {
+  process.stdout.write(character)
+  if (pixel % LINE_WIDTH === 0) process.stdout.write('\n')
+  pixel++
 }
 
 async function main () {
@@ -34,7 +43,6 @@ async function main () {
   for await (const line of rl) {
     parseLine(line)
   }
-  console.log({ total })
 }
 
 main()
